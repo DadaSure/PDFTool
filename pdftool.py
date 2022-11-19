fc3dca19b557a3074c84473fd287d3fc96494c70
wangshuo <wangice2010@163.com>
2022-11-19 12:47:32 +0800

    结构改造

---
+++ pdftool.py
@@ -0,0 +1,13 @@
+import mod1_create
+import mod2_merge
+import mod3_batch_processing
+
+inputFile = "0"
+
+operationCode = -1
+#1-PDF_Create(imgdir),2-PDF_Merge(pdfdir),3-PDF_division(divdoc, range)
+#4-
+
+
+
+if operationCode == 1:
